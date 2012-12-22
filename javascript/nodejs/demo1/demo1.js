1274cfbd6e1d08ad2197e74e3483f3dff8673df6
haiqing.bu@gmail.com <buha@BUHA1.asiapacific.hpqcorp.net>
2012-12-22 09:19:42 +0800

    make a bit change

--- javascript/nodejs/demo1/demo1.js
+++ javascript/nodejs/demo1/demo1.js
@@ -2,5 +2,29 @@ var fs = require('fs'),
    sys = require('sys');
 
  // hello world in Node
+ console.log('====================================');
+ console.log('**** hello world up and running ****');
  console.log("hello world");
- console.log("a test");
+ 
+ // Node `process` object
+ console.log('\n');
+ console.log('====================================');
+ console.log('**** use process global object ****');
+ console.log(process.execPath);
+ console.log(process.version);
+ console.log(process.platform);
+ 
+
+// child_process
+console.log('\n');
+console.log('====================================');
+console.log('**** use child_process module ****');
+var exec = require('child_process').exec;
+
+cmd = 'ls -lrt'
+exec(cmd, function(error, stdio, stderr) { 
+  console.log('s: ' + stdio + '\n');
+  // console.log('e: ' + stderr + '\n'); 
+});
+
+

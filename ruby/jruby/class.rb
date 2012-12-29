035443f12d81303c8bc8c358500ab2992eae41c0
haiqing.bu@gmail.com <buha@BUHA1.asiapacific.hpqcorp.net>
2012-12-29 19:28:01 +0800

    add new test case

--- ruby/jruby/class.rb
+++ ruby/jruby/class.rb
@@ -1,7 +1,19 @@
 class A
  attr_accessor :name
+
+ # static method
+ def self.test
+ 	 'test'
+ end
+
+ def test1
+ 	 'test1'
+ end
 end
 
 a = A.new
 a.name ="buhaiqing"
-puts a.name
+
+puts "name is #{a.name}"
+puts a.test1
+puts A.test

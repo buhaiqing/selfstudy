048396eb2fde91b1a400a05d0557fd06a841c811
haiqing.bu@gmail.com <buha@BUHA1.asiapacific.hpqcorp.net>
2012-12-29 18:59:46 +0800

    add new test cases

---
+++ ruby/jruby/class.rb
@@ -0,0 +1,7 @@
+class A
+ attr_accessor :name
+end
+
+a = A.new
+a.name ="buhaiqing"
+puts a.name
---
+++ ruby/jruby/java_integration.rb
@@ -0,0 +1,11 @@
+# require 'java'
+
+java_import java.util.ArrayList
+	
+
+list = ArrayList.new
+list  <<  11
+list <<   22
+puts list
+
+

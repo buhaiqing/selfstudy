59556b5ad07f59a798026c53cdd9fcb61b290e0d
buhaiqing <haiqing.bu@gmail.com>
2014-02-20 15:59:36 +0800

    update

--- groovy/HPSM/automation/checksvnCommitComments.groovy
+++ groovy/HPSM/automation/checksvnCommitComments.groovy
@@ -1,4 +1,3 @@
-
 // Author: Bu Hai-Qing (hai-qing.bu@hp.com)
 // Usage: groovy checksvnCommitComments.groovy start-date end-date
 //   example: svn log -r {2014-02-02}:{2014-02-10} --xml
@@ -6,6 +5,7 @@
 
 // the path you want to scan
 
+import static groovyx.gpars.GParsPool.withPool
 
 if (args.length != 2){
   Usage()
@@ -21,37 +21,38 @@ projs = [
 [project:"UCMDB in 940" , logfile:"UCMDB_sm940.log",scan_path:"Integration\\UCMDB\\SM940"],
 [project:"PD Request" , logfile:"pd-request.log",scan_path:"ApplicationTests\\RequestManagement"]
 ]
+withPool 16,{
+  projs.eachParallel{
+      def project = it['project']
+      def logfile = it['logfile']
+      def path = it['scan_path']
+      
+      File f = new File(logfile)
+      PrintWriter pw = new PrintWriter(f)
+      
+      // svn command to get xml format log entries
+      command = "svn log -r {${startdate}}:{${enddate}} --xml  ${path}"
+      //println "command is ${command}"
+      text =  command.execute().text
+      // parse the xml output and filter out the result set that follow our rules
+      def log  = new XmlSlurper().parseText(text)
+      def issue_list = log.logentry.grep{
+          def msg = it['msg']
+          def pattern = 'http://reviewboard.chn.hp.com/reviews/r'
+          !msg.toString().contains(pattern)
+      }
 
-projs.each{
-    def project = it['project']
-    def logfile = it['logfile']
-    def path = it['scan_path']
-    
-    File f = new File(logfile)
-    PrintWriter pw = new PrintWriter(f)
-    
-    // svn command to get xml format log entries
-    command = "svn log -r {${startdate}}:{${enddate}} --xml  ${path}"
-    //println "command is ${command}"
-    text =  command.execute().text
-    // parse the xml output and filter out the result set that follow our rules
-    def log  = new XmlSlurper().parseText(text)
-    def issue_list = log.logentry.grep{
-        def msg = it['msg']
-        def pattern = 'http://reviewboard.chn.hp.com/reviews/r'
-        !msg.toString().contains(pattern)
-    }
+      issue_list.each {
+          pw.printf "Author : %-10s; revision : %7s;\n",it['author'], it.@'revision'
 
-    issue_list.each {
-        pw.printf "Author : %-10s; revision : %7s;\n",it['author'], it.@'revision'
+      }
+      pw.close()
+      if(issue_list.size != 0){
+        println "there are issues found on ** ${project} ** project.please check log file **${logfile}** for detail"
+      }
+  }
 
-    }
-    pw.close()
-    if(issue_list.size != 0){
-      println "there are issues found on ** ${project} ** project.please check log file **${logfile}** for detail"
-    }
 }
-
 def Usage(){
   println """
   the arguments length is not correct!!

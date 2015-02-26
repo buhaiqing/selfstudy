b44391674fbf17f317d07b9128fba98e2827def1
buhaiqing <haiqing.bu@gmail.com>
2015-02-26 11:45:32 +0800

    update a bit

--- shell/remove_all_file.sh
+++ shell/remove_all_file.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 ## Delete all files except file1 ##
 rm !(file1)
+#OR
+find . -not -name "file1" -delete
 
 ## Delete all file except file1 and file2 ##
 rm !(file1|file2)
+#OR
+find . -not -name "file1" -not -name "file2" -delete
 
 ## Delete all file except all zip files ##
 rm !(*.zip)
+#OR
+find . -not -name "*.zip" -delete
 

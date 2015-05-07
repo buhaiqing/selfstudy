5030aab4c6b5e6270fa5e8749ddd2d2505e69f9e
buhaiqing <haiqing.bu@gmail.com>
2015-05-07 16:06:02 +0800

    test

---
+++ powershell/HPSM/get_Quarantined_JS_Files.ps1
@@ -0,0 +1,29 @@
+﻿#####################################################################################
+#Tested on PowerShell v3.0
+#####################################################################################
+<#
+ # This script is used to report script files which are Quarantined
+ # Message From Jerry:
+ 1)	We will regard any JS test with 1st line comment of 
+   //Quarantined
+   as disabled tests for statistics purpose.
+  2)	Meanwhile use xdescribe(…) in test body to skip its running in Jasmine framework.
+
+Please follow the rules above in subsequent tests developing.
+  
+#>
+# you need to configure path you want to scan
+Param([String]$root_folder=".")
+$Quarantined_js_files = @()
+$(dir -Path $root_folder -Filter *.js -Recurse) |%{ 
+$content = cat $_.FullName |Out-String
+ 
+if ($content.StartsWith("//Quarantined")){
+  $Quarantined_js_files += $_.FullName
+}
+}
+$Quarantined_js_files|ft -AutoSize
+
+
+
+

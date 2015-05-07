2cee282a6866de0e526f8e90e250b4c887636369
buhaiqing <haiqing.bu@gmail.com>
2015-05-07 15:06:59 +0800

    add powershell script

---
+++ powershell/HPSM/getHummerExecutionStatus.ps1
@@ -0,0 +1,17 @@
+﻿#####################################################################################
+#Tested on PowerShell v3.0
+#####################################################################################
+$build_id = "20150507091513"
+$res = (Invoke-RestMethod "http://16.186.72.7/api/task_list?build=$build_id")| %{
+$passed = $_.passing_ratio.passed
+$failed_case = $_.passing_ratio.failed
+$case_number = $_.passing_ratio.case_number
+$passing_ratio = $_.passing_ratio."passed%"
+
+$_ |add-Member -MemberType NoteProperty -Name passed -Value $passed
+$_| Add-Member -MemberType NoteProperty -Name failed $failed_case
+$_| Add-Member -MemberType NoteProperty -Name case_number $case_number
+$_| Add-Member -MemberType NoteProperty -Name passed% $passing_ratio
+
+$_
+} |Sort-Object -Property group|Out-GridView
